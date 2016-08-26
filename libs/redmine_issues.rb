146c0ce68621afca0619ade02acc8302918fe43b
Oskar Calvo <calvo.oskar@gmail.com>
2016-08-26 15:06:45 +0200

    * Refactorización de las llamadas a la API de Redminen en la clase RedmineIssues. * Actualización de Gemfile al añadir la gema de Faraday y Gemfile.lock.

--- libs/redmine_issues.rb
+++ libs/redmine_issues.rb
@@ -2,10 +2,7 @@ class RedmineIssues
 
   def get_issues (path)
 
-
     response = RedmineConnect2API.new.connect(path)
-
-
 
     case response
     when  Net::HTTPSuccess then
@@ -15,15 +12,12 @@ class RedmineIssues
       return nil
     end
 
-
   end
 
   def get_project_users(path, session)
 
     response = RedmineConnect2API.new.connect(path, {user: session[:loginname], pass: session[:loginpass] })
     #binding.pry
-
-
     case response
     when  Net::HTTPSuccess then
       data = JSON.parse(response.body)
@@ -37,14 +31,8 @@ class RedmineIssues
 
   def get_issue_priorities(path, session)
 
-    uri = URI.parse(path)
-    req = Net::HTTP::Get.new(uri)
-    response = nil
-    req.basic_auth session[:loginname], session[:loginpass]
-    res = Net::HTTP.start(uri.hostname, uri.port) {|http|   response = http.request(req)  }
+    response = RedmineConnect2API.new.connect(path, {user: session[:loginname], pass: session[:loginpass]})
     #binding.pry
-
-
     case response
     when  Net::HTTPSuccess then
       data = JSON.parse(response.body)
@@ -57,14 +45,8 @@ class RedmineIssues
 
   def getissuedata(path, session)
 
-    uri = URI.parse(path)
-    req = Net::HTTP::Get.new(uri)
-    response = nil
-    req.basic_auth session[:loginname], session[:loginpass]
-    res = Net::HTTP.start(uri.hostname, uri.port) {|http|   response = http.request(req)  }
+    response = RedmineConnect2API.new.connect(path, {user: session[:loginname], pass: session[:loginpass]})
     #binding.pry
-
-
     case response
     when  Net::HTTPSuccess then
       data = JSON.parse(response.body)
@@ -77,14 +59,8 @@ class RedmineIssues
 
   def get_issuestatus(path, session)
 
-    uri = URI.parse(path)
-    req = Net::HTTP::Get.new(uri)
-    response = nil
-    req.basic_auth session[:loginname], session[:loginpass]
-    res = Net::HTTP.start(uri.hostname, uri.port) {|http|   response = http.request(req)  }
+    response = RedmineConnect2API.new.connect(path, {user: session[:loginname], pass: session[:loginpass]})
     #binding.pry
-
-
     case response
     when  Net::HTTPSuccess then
       data = JSON.parse(response.body)
