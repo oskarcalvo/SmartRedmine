a20927cb7b9529c402a87f0ec29d7f8142f97e15
Oskar Calvo <calvo.oskar@gmail.com>
2016-08-31 15:19:47 +0200

    Actualización del js para cargar bien los pickedate

--- asset/js/easyredmine.js
+++ asset/js/easyredmine.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 	$(".saveissue").click(function () {
-  	
+
 		console.log($(this));
   	//var issue = $(this).attr("#data-issue");
   	var issue = $(this).attr('data-id');
@@ -12,5 +12,32 @@ $(document).ready(function () {
   	//alert("Handler for .click() called." + issue);
 
   });
+});
+
+$(document).ready(function () {
+	var simplemde = new SimpleMDE({
+		autofocus: true,
+		toolbar: ["bold", "italic", "heading", "|", "quote","code", "|", "unordered-list","ordered-list","table"],
+		element: document.getElementById("textarea_notes"),
+		showIcons: ["code", "table"],
+		spellChecker: false
+	});
+
+
+});
+
+
+$(document).ready(function(){
+
+	$('.issue_start_date').pickadate({
+		firstDay: 1,
+		formatSubmit: 'd-m-yyyy',
+		format: 'd-m-yyyy',
+	});
+	$('.issue_due_date').pickadate({
+		firstDay: 1,
+		formatSubmit: 'd-m-yyyy',
+		format: 'd-m-yyyy',
+	});
 
 });
